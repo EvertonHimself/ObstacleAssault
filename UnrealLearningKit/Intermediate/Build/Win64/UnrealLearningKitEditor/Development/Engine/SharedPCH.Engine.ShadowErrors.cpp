29fdd8f327361cc3086bfd1dcb9425856b6541e9
EvertonHimself <evertondeoliveira@gmail.com>
2022-12-31 13:54:54 -0300

    Created moving platform.
    I've created a moving platform, imported some assets kits from the Unreal Store, following the Unreal C++ Udemy course.

---
+++ UnrealLearningKit/Intermediate/Build/Win64/UnrealLearningKitEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30147
+#include "C:/Users/evert/Documents/GitHub/ObstacleAssault/UnrealLearningKit/Intermediate/Build/Win64/UnrealLearningKitEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
